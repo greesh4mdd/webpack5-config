52957189f137b9a27e2e8c7710328c733012fd47
Mhamed Mrini <abu3alae@gmail.com>
2021-04-17 00:36:27 +0000

    add CSS, SASS, POSTCSS, HotReloading and prefix browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+
 let mode = "development"
+let target = "web"
 
 if(process.env.MODE_ENV === "production"){
     mode = "production"
+    target = "browserslist"
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     module: {
         rules: [
+            {
+                test: /\.(s[ac]|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader", 
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
@@ -19,9 +34,12 @@ module.exports = {
         ]
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool: "source-map",
 
     devServer: {
-        contentBase: "./dist"
+        contentBase: "./dist",
+        hot: true
     }
 }
